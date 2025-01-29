1d2625f166a767c19a7b59980822b4b941e6ff5b
yoyfordsd <fabroayoy00@gmail.com>
2025-01-29 20:00:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -102,3 +102,17 @@ const sections = document.querySelectorAll('body');
     sections.forEach(section => {
       observer.observe(section);
     });
+    window.addEventListener('scroll', () => {
+        const navbar = document.querySelector('.navbar');
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+    const mobileMenu = document.getElementById('mobileMenu');
+const menuIcon = document.getElementById('menuIcon');
+
+menuIcon.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+});
